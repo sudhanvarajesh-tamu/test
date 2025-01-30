8820981fcc8981879bb80409617ee5c036571d7b
root <root@localhost>
2025-01-30 14:48:21 +0000

    added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
 #  has_many :posts
 #  has_many :comments
 #add another comment. Yeah
-
-
+    validates :first_name, presence: true
+    validates :last_name, presence: true
 end
